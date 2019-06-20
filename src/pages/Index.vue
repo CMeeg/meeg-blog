b7e4221ff21a37ced919c8fb415d0109f98a2986
Chris Meagher <cpmeeg@gmail.com>
2019-06-20 23:38:25 +0100

    Added basic listing of Articles to Home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,11 +3,18 @@
       <h1>{{ homePage.title }}</h1>
 
       <div v-html="homePage.summary" />
+
+      <ul>
+        <li v-for="article in $page.latestArticles.edges" :key="article.node.id">
+          <g-link :to="`article/${article.node.slug}`">{{ article.node.title }}</g-link>
+        </li>
+      </ul>
+      <Pager :info="$page.latestArticles.pageInfo"/>
   </Layout>
 </template>
 
 <page-query>
-query Home {
+query Home($page: Int) {
   home: allLandingPage(filter: { codename: { eq: "home" }}, limit: 1) {
     edges {
       node {
@@ -19,11 +26,34 @@ query Home {
       }
     }
 	}
+  latestArticles: allArticle(perPage: 10, page: $page) @paginate {
+    pageInfo {
+      totalPages
+      currentPage
+    }
+    edges {
+      node {
+        id,
+        title,
+        author,
+        summary,
+        slug
+        # articleTopics,
+        # date,
+        # lastUpdated
+      }
+    }
+  }
 }
 </page-query>
 
 <script>
+import { Pager } from 'gridsome'
+
 export default {
+  components: {
+    Pager
+  },
   metaInfo () {
     return {
       title: this.homePage.metaTitle || 'Home',
