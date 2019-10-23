edab26c9f7506230eb786319d5582c96f7019496
Chris Meagher <cpmeeg@gmail.com>
2019-10-24 00:44:27 +0100

    Replace Twitter and GitHub logos with inline Feather SVG icons

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,15 +2,23 @@
   <div>
     <header class="relative top-0 flex justify-between md:justify-center px-4 py-2 border-red-600 border-t-4 bg-white shadow-md md:py-4">
       <g-link to="/" class="flex text-black hover:no-underline focus:no-underline">
-        <g-image src="~/assets/img/cmeeg.jpg" width="200" alt="Chris Meagher" class="w-16 h-16 rounded-full border border-gray-400 mr-2" />
+        <g-image src="@/assets/img/chris-meagher.jpg" width="100" alt="Chris Meagher" class="w-16 h-16 rounded-full border border-gray-400 mr-2" />
         <p class="self-center leading-none pt-3 pr-4 pb-1 border-b-2 border-white hover:border-gray-400 focus:border-gray-400">
           <strong class="font-serif text-xl font-normal">Chris Meagher</strong><br>
           <span class="font-mono italic text-gray-600">{{ siteName }}</span>
         </p>
       </g-link>
       <p class="flex-shrink self-center md:flex-none md:absolute md:right-0 md:pr-4">
-        <a :href="`https://twitter.com/${ twitterUser }`"><g-image src="~/assets/img/twitter.svg" alt="Twitter" class="w-6 h-6 m-1" /></a>
-        <a :href="`https://github.com/${ gitHubUser }`"><g-image src="~/assets/img/github.png" alt="GitHub" class="w-6 h-6 m-1" /></a>
+        <a :href="`https://twitter.com/${ twitterUser }`">
+          <svg class="stroke-current text-blue-400 h-6 w-6 mb-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+          </svg>
+        </a>
+        <a :href="`https://github.com/${ gitHubUser }`">
+          <svg class="stroke-current text-gray-800 h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+          </svg>
+        </a>
       </p>
     </header>
     <main class="p-4 pb-2">
