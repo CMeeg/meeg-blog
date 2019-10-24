6c8e4967861385e6904d1b8c23856eeb59dae253
Chris Meagher <cpmeeg@gmail.com>
2019-10-25 00:04:51 +0100

    Styled Article Series template

--- src/templates/ArticleSeries.vue
+++ src/templates/ArticleSeries.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    <h1>{{ pageNode.title }}</h1>
+    <page-intro :title="pageNode.title">
+      <div slot="body">
+        <p class="text-gray-600 mb-4 flex align-middle">
+          <svg class="stroke-current h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+            <line x1="16" y1="2" x2="16" y2="6" />
+            <line x1="8" y1="2" x2="8" y2="6" />
+            <line x1="3" y1="10" x2="21" y2="10" />
+          </svg>
+          <time :datetime="pageNode.lastUpdated" class="pl-2">{{ new Date(pageNode.lastUpdated).toDateString() }}</time>
+        </p>
 
-    <p>Updated <time :datetime="pageNode.lastUpdated">{{ pageNode.lastUpdated }}</time></p>
-
-    <rich-text :html="pageNode.summary" />
+        <div class="font-serif italic md:text-lg leading-tight">
+          <rich-text :html="pageNode.summary" />
+        </div>
+      </div>
+    </page-intro>
 
     <article-summary-list :articles="articles" />
   </div>
@@ -42,12 +54,14 @@ query ArticleSeries ($id: ID!) {
 </page-query>
 
 <script>
+import PageIntro from '@/components/PageIntro.vue';
 import RichText from '@/components/kontent/RichText.vue';
 import ArticleSummaryList from '@/components/articles/ArticleSummaryList.vue';
 import metadata from '@/mixins/Metadata';
 
 export default {
   components: {
+    PageIntro,
     RichText,
     ArticleSummaryList
   },
