05c45761a3580f37be7b5acec7e45a7e6332c67b
Chris Meagher <cpmeeg@gmail.com>
2019-10-24 23:26:07 +0100

    Styled 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <h1>{{ pageNode.title }}</h1>
+    <page-intro :title="pageNode.title" />
+
+    <div class="rich-text">
+      <p>Sorry that you've ended up here... you may want to try:</p>
+
+      <ul>
+        <li>Browsing through the <g-link to="/articles/">articles</g-link> list</li>
+        <li>Browsing through the <g-link to="/series/">articles series</g-link> list</li>
+        <li>Finding articles by <g-link to="/tags/">tag</g-link></li>
+        <li>Searching for the article via your preferred search engine</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import PageIntro from '@/components/PageIntro.vue';
 import metadata from '@/mixins/Metadata';
 
 export default {
+  components: {
+    PageIntro
+  },
   mixins: [
     metadata
   ],
