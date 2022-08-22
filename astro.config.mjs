5bd1e1f7a8d516bf4f5e391d3517520c54df8003
Chris Meagher <cpmeeg@gmail.com>
2022-08-22 16:09:42 +0100

    Add https support
    This is required by Storyblok v2 preview and visual editor.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config'
+import { readFileSync } from 'fs'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  vite: {
+    server: {
+      https: {
+        key: readFileSync('./localhost-key.pem', 'utf8'),
+        cert: readFileSync('./localhost.pem', 'utf8')
+      }
+    }
+  }
+})
