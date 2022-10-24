7ac2df139a1c961bb5057b1b723aed86e5327e2b
Chris Meagher <cpmeeg@gmail.com>
2022-10-24 12:28:51 +0100

    Add narrow viewport nav styles

--- src/features/common/DefaultLayout/Header.tsx
+++ src/features/common/DefaultLayout/Header.tsx
@@ -6,6 +6,7 @@ import Image from '~/features/common/Image'
 import Link from '~/features/common/Link'
 import { ChrisMeagher } from '~/svg/index'
 import { getImageFromAsset } from '../media'
+import ThemeSwitcher from '~/features/themes/ThemeSwitcher'
 import styles from './header.module.scss'
 
 export interface Props {
@@ -17,8 +18,8 @@ export default function Header({ logo, mainNav }: Props) {
   const image = getImageFromAsset(logo, { resize: { width: 140 }, quality: 80 })
 
   return (
-    <header>
-      <div className={styles.logo}>
+    <header className={styles.header}>
+      <a href="/" className={styles['logo']}>
         <span className={styles['logo-img-border']}>
           <Image
             className={styles['logo-img']}
@@ -28,22 +29,39 @@ export default function Header({ logo, mainNav }: Props) {
           />
         </span>
         <ChrisMeagher className={styles['logo-text']} />
-      </div>
+      </a>
 
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
+      <nav role="navigation" className={styles.nav}>
+        <div className={styles['nav-container']}>
+          <input
+            type="checkbox"
+            className={styles['nav-toggle']}
+            id="nav-primary-toggle"
+          />
+          <label htmlFor="nav-primary-toggle" className="sr-only">
+            Menu
+          </label>
 
-          {mainNav.map((item) => {
-            return (
-              <li key={item._uid}>
-                <Link link={item.link}>{item.name}</Link>
+          <span className={styles['nav-icon']} />
+
+          <div className={styles['nav-content']}>
+            <ul className={styles['nav-primary']}>
+              <li>
+                <a href="/">Home</a>
               </li>
-            )
-          })}
-        </ul>
+
+              {mainNav.map((item) => {
+                return (
+                  <li key={item._uid}>
+                    <Link link={item.link}>{item.name}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+
+            <ThemeSwitcher />
+          </div>
+        </div>
       </nav>
     </header>
   )
