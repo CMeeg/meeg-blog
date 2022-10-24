752effd9e5a05b63cbb068babae06bb5f023c8bb
Chris Meagher <cpmeeg@gmail.com>
2022-10-24 13:09:37 +0100

    Fix runtime error on production build from react-use import

--- src/features/themes/ThemeSwitcher/index.tsx
+++ src/features/themes/ThemeSwitcher/index.tsx
@@ -1,11 +1,16 @@
 import { useEffect } from 'react'
-import { useMedia, useCookie } from 'react-use'
+// This react-use import is done like this to workaround some incompatibility between the library and Astro - if you import react-use as "normal" then you get a runtime error on a production build
+// https://github.com/withastro/astro/issues/3174
+import * as reactUse from 'react-use'
+const { useMedia, useCookie } =
+  (reactUse as unknown as { default: typeof reactUse }).default || reactUse
 import { themes, defaultThemeName, themeCookieName } from '~/features/themes'
 import { Sun, Moon } from '~/svg/icons'
 import styles from './index.module.scss'
 
 export default function ThemeSwitcher() {
   const [value, updateCookie] = useCookie(themeCookieName)
+  // In dev `useMedia` will complain about not having a `defaultState`, but we don't want to set a default state because we can't assume the value of `prefers-color-scheme` - instead we handle it in `useEffect`
   const prefersDarkColorScheme = useMedia('(prefers-color-scheme: dark)')
 
   const toggleTheme = (preference?: string) => {
@@ -53,14 +58,14 @@ export default function ThemeSwitcher() {
   }
 
   useEffect(() => {
-    if (!value) {
+    if (typeof prefersDarkColorScheme !== 'undefined') {
       const colorSchemePreference = prefersDarkColorScheme
         ? themes.dark.name
         : themes.light.name
 
       toggleTheme(colorSchemePreference)
     }
-  }, [])
+  }, [prefersDarkColorScheme])
 
   return (
     <div className={styles['theme-switcher']}>
