bfd60e6af98339a26a5ef060888398d973882312
Chris Meagher <cpmeeg@gmail.com>
2022-11-03 19:15:44 +0000

    Add tag page

---
+++ src/features/blog/ArticleTagPage/index.tsx
@@ -0,0 +1,39 @@
+import type {
+  PageHeadingStoryblok,
+  ArticleListingStoryblok
+} from '~/features/storyblok/types/components'
+import type { ArticleStory } from '~/features/blog/api'
+import PageHeading from '~/features/common/PageHeading'
+import ArticleListing from '~/features/blog/ArticleListing'
+
+export interface Props {
+  tag: string
+  articles: ArticleStory[]
+}
+
+export default function ArticleTagPage({ tag, articles }: Props) {
+  const headingBlok: PageHeadingStoryblok = {
+    title: tag,
+    component: 'page_heading',
+    _uid: ''
+  }
+
+  const articlesBlok: ArticleListingStoryblok = {
+    articles,
+    per_page: 12,
+    component: 'article_listing',
+    _uid: ''
+  }
+
+  return (
+    <>
+      <PageHeading blok={headingBlok}>
+        <p>
+          Articles about <i>{tag}</i> can be found below.
+        </p>
+      </PageHeading>
+
+      <ArticleListing blok={articlesBlok} />
+    </>
+  )
+}
--- src/features/common/PageHeading/index.tsx
+++ src/features/common/PageHeading/index.tsx
@@ -4,17 +4,26 @@ import styles from './index.module.scss'
 
 export interface Props {
   blok: PageHeadingStoryblok
+  children: React.ReactNode
 }
 
-export default function PageHeading({ blok }: Props) {
+export default function PageHeading({ blok, children }: Props) {
   return (
     <div className={styles['page-heading']}>
       <h1 className={styles['page-heading-title']}>{blok.title}</h1>
 
-      <RichText
-        className={styles['page-heading-description']}
-        document={blok.intro}
-      />
+      {blok.intro && (
+        <RichText
+          className={styles['page-heading-description']}
+          document={blok.intro}
+        />
+      )}
+
+      {children && (
+        <div className={`prose ${styles['page-heading-description']}`}>
+          {children}
+        </div>
+      )}
     </div>
   )
 }
