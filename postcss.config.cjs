fb7cbedc45046951935ab496e8d831eeb083bbf0
Chris Meagher <cpmeeg@gmail.com>
2022-11-06 14:19:31 +0000

    Fix postcss not transforming custom media queries from open props

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -6,7 +6,12 @@ module.exports = {
   plugins: [
     postCssPresetEnv({
       stage: 2,
-      browsers: 'last 2 versions or > 3%'
+      browsers: 'last 2 versions or > 3%',
+      features: {
+        'custom-media-queries': {
+          importFrom: 'node_modules/open-props/media.min.css'
+        }
+      }
     }),
     postCssFlexbugsFixes
   ]
